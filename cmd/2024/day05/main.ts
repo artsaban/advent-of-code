b750356f1fd78a51cab67c24ecf5ad056e633c10
Artem Shabanov <artem.a.shabanov@gmail.com>
2024-12-06 14:33:42 +0700

    2024 day 05 part 2

--- cmd/2024/day05/main.ts
+++ cmd/2024/day05/main.ts
@@ -6,31 +6,51 @@ const [rulesRaw, updatesRaw] = input.split("\n\n");
 const rules = rulesRaw.split("\n").map((x) => x.split("|").map(Number));
 const updates = updatesRaw.split("\n").map((x) => x.split(",").map(Number));
 
+const orderGraph = new DefaultDict<number, number[]>(() => []);
+for (const [from, to] of rules) {
+  orderGraph.get(to).push(from);
+}
+
 function part1(): number {
-  const graph = new DefaultDict<number, number[]>(() => []);
-  for (const [from, to] of rules) {
-    graph.get(to).push(from);
-  }
-
-  function isCorrect(update: number[]) {
-    for (let i = 0; i < update.length; i++) {
-      const u = update[i];
-      const after = update.slice(i + 1);
-      const requiredPages = graph.get(u);
-      if (requiredPages.some((x) => after.includes(x))) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   let result = 0;
   for (const update of updates) {
-    if (isCorrect(update)) {
+    if (isCorrectUpdate(update, orderGraph)) {
       result += update[Math.floor(update.length / 2)];
     }
   }
   return result;
 }
 
+function part2(): number {
+  function updateComparator(a: number, b: number): -1 | 0 | 1 {
+    const beforeB = orderGraph.get(b);
+    if (beforeB.includes(a)) return -1;
+
+    const beforeA = orderGraph.get(a);
+    if (beforeA.includes(b)) return 1;
+
+    return 0;
+  }
+
+  let result = 0;
+  for (const update of updates) {
+    if (isCorrectUpdate(update, orderGraph)) continue;
+    result += update.sort(updateComparator)[Math.floor(update.length / 2)];
+  }
+  return result;
+}
+
+function isCorrectUpdate(update: number[], graph: DefaultDict<number, number[]>) {
+  for (let i = 0; i < update.length; i++) {
+    const u = update[i];
+    const after = update.slice(i + 1);
+    const requiredPages = graph.get(u);
+    if (requiredPages.some((x) => after.includes(x))) {
+      return false;
+    }
+  }
+  return true;
+}
+
 console.log("Part 1:", part1());
+console.log("Part 2:", part2());
